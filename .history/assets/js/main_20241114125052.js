8d3a1a8d97028a88e82863ac1f90d1f3265651ec
Ramiro <castanedaramiro.2000@gmail.com>
2024-11-14 13:07:11 -0300

    Agregue categorias para los productos y arreglos responsive

--- .history/assets/js/main_20241114125052.js
+++ .history/assets/js/main_20241114125052.js
@@ -87,7 +87,7 @@ const sr = ScrollReveal({
 sr.reveal(`.home__header, .section__title, .home__img`,{delay: 600})
 sr.reveal(`.home__footer, .button--flex`,{delay: 700})
 
-sr.reveal(`.sponsor__img, .products__category, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
+sr.reveal(`.sponsor__img, .products.products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
 sr.reveal(`.specs__content, .discount__animate`,{origin: 'left', interval: 100})
 sr.reveal(`.specs__img, .discount__img`,{origin: 'right'})
 sr.reveal(`.case__img`,{origin: 'top'})
